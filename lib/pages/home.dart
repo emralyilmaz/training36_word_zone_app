ab2a9a084aa8e975220edf06d92347444731bcd7
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-14 15:34:28 +0300

    Tarih Gösterme

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,6 +13,7 @@ class _HomeState extends State<Home> {
     routeVeriler = ModalRoute.of(context).settings.arguments;
     print(routeVeriler);
     return Scaffold(
+      backgroundColor: Color.fromRGBO(242, 233, 225, 1),
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(203, 232, 107, 1),
         title: Text("Dünya Saatleri",
@@ -20,15 +21,40 @@ class _HomeState extends State<Home> {
       ),
       body: SafeArea(
           child: Center(
-        child: Column(
-          children: <Widget>[
-            FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/konum");
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text("Konum Düzenle"))
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(top: 120),
+          child: Column(
+            children: <Widget>[
+              FlatButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/konum");
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text(
+                    "Konum Düzenle",
+                    style: TextStyle(color: Color.fromRGBO(28, 20, 13, 1)),
+                  )),
+              SizedBox(
+                height: 100.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    routeVeriler["konum"],
+                    style: TextStyle(fontSize: 25.0, letterSpacing: 2.0),
+                  ),
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  Text(
+                    routeVeriler["zaman"],
+                    style: TextStyle(fontSize: 60.0, letterSpacing: 2.0),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       )),
     );

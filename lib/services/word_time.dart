217600b902510a30e60160bb37855d00edcc4ab8
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-14 12:55:36 +0300

    World Time Class Oluşturma

---
+++ lib/services/word_time.dart
@@ -0,0 +1,23 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class WordTime {
+  String konum;
+  String zaman;
+  String url;
+  String bayrak;
+
+  WordTime({this.konum, this.zaman, this.url});
+
+  void getZaman() async {
+    http.Response res =
+        await http.get("http://worldtimeapi.org/api/timezone/$url");
+    Map veri = jsonDecode(res.body);
+    String dateTime = veri["datetime"];
+    DateTime now = DateTime.parse(dateTime);
+    String offset =
+        veri["utc_offset"].substring(1, 3); // substing parça al gibi çalışıyor
+    now = now.add(Duration(hours: int.parse(offset)));
+    zaman = now.toString()
+  }
+}
